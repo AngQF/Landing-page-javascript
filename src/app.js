2da8b19f13b54551c6f4ac5ca4acae43e3844daf
AngQF <angela.qf@hotmail.com>
2022-03-17 22:55:03 +0000

    ejercicio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.getElementById("regalazo").innerHTML = regalo();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+const regalo = () => {
+  let prenda = ["CAMISETA", "CAMISA", "CHAQUETA"];
+  let color = ["NEGRA", "MARRÓN", "BLANCA"];
+  let marca = ["DENMARK"];
+
+  let regalazo = "";
+
+  let prendaindex = Math.floor(Math.random() * prenda.length);
+  let colorindex = Math.floor(Math.random() * color.length);
+  let marcaindex = Math.floor(Math.random() * marca.length);
+
+  return (
+    prenda[prendaindex] + ` ` + color[colorindex] + ` ` + marca[marcaindex]
+  );
 };
